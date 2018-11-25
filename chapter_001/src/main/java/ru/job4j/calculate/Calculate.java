58e26723718eb815e2c0efb0255d4622b2460442
nikolayzhuykov <kishinx@rambler.ru>
2018-11-26 02:05:16 +0600

    1.3. JavaDoc. Code convention[#83372]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,17 +1,20 @@
 package ru.job4j.calculate;
 
 /**
- *Calculate
+ * Calculate.
  *
- *@author Nikolay Zhuykov (kishinx@rambler.ru)
- *@since 25.11.2018
- *@version 1
+ * @author Nikolay Zhuykov (kishinx@rambler.ru)
+ * @since 25.11.2018
+ * @version 1
  */
 
 public class Calculate {	
-	
+	/**
+	 * Main.
+	 * @param args - args.
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
-
+	
 }
